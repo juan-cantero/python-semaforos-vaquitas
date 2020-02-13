2f35e3f1bfc1527a0316227107c6ee06c99e62e8
sebasped <sebasped@disroot.org>
2020-02-13 08:56:47 -0300

    preliminar vaquitas como ejercicio

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# python-semaforos-parte2
+# Semáforos parte 2
+
+## vaquitas.py
+
+Vamos a simular que hay vacas yendo por un camino, todas para el mismo lado, y se topan con un puente.
+Las vacas caminan a diferentes velocidades, y el puente no soporta el paso de más de una vaca a la vez.
+
+Mirá la implementación que esta ahora, correla algunas veces, y asegurate de entender a grandes rasgos el código.
+
+Ahora agregale semáforos para asegurar que haya solamente una vaca cruzando el puente a la vez.
+
+### Variantes de vaquitas.py
+
+Ahora podés probar algunas variantes. En principio implementalas tomando como punto de partida la versión que hiciste en el punto anterior.
+
+* Como máximo haya 2 vacas cruzando el puente a la vez. Si te animás, que sean N.
+* Que haya dos puentes.
+* Que haya vacas caminando en ambos sentidos.
---
+++ vaquitas.py
@@ -0,0 +1,46 @@
+import os
+import random
+import time
+import threading
+
+inicioPuente = 10
+largoPuente = 20
+
+class Vaca(threading.Thread):
+  def __init__(self):
+    super().__init__()
+    self.posicion = 0
+    self.velocidad = random.uniform(0.1, 0.5)
+
+  def avanzar(self):
+    time.sleep(self.velocidad)
+    self.posicion += 1
+
+  def dibujar(self):
+    print(' ' * self.posicion + "🐮")
+
+  def run(self):
+    while(True):
+      self.avanzar()
+
+vacas = []
+for i in range(5):
+  v = Vaca()
+  vacas.append(v)
+  v.start()
+
+def cls():
+  os.system('cls' if os.name=='nt' else 'clear')
+
+def dibujarPuente():
+  print(' ' * inicioPuente + '=' * largoPuente)
+
+while(True):
+  cls()
+  print('Apretá Ctrl + C varias veces para salir...')
+  print()
+  dibujarPuente()
+  for v in vacas:
+    v.dibujar()
+  dibujarPuente()
+  time.sleep(0.2)
